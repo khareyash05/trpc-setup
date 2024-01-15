9832340a6f565f7059e53bbd9726c486bbfb740b
Yash Khare <yash2010118@akgec.ac.in>
2024-01-15 12:43:04 +0530

    setup trpc

---
+++ client/index.ts
@@ -0,0 +1,26 @@
+import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
+import type { AppRouter } from '../server';
+import { User } from '../server/db';
+
+const trpc = createTRPCProxyClient<AppRouter>({
+  links: [
+    httpBatchLink({
+      url: 'http://localhost:3000',
+      async headers() {
+        return {
+            Authorization: "Bearer 1"
+        }
+      },
+    }),
+  ],
+});
+
+async function main() {
+    const user = await trpc.user.signup.mutate({
+        username: "harkirat@gmail.com",
+        password: "!23456"
+    });
+    console.log(user.token);
+}
+
+main();
